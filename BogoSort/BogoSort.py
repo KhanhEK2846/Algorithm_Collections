4eae316adc60a66638d437abec73a6803975a2a6
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2024-02-25 15:14:47 +0700

    Bozo sort

--- BogoSort/BogoSort.py
+++ BogoSort/BogoSort.py
@@ -1,16 +1,24 @@
 import random
 
-def is_sorted(data):
-    for i in range(len(data) - 1):
-        if data[i] > data[i + 1]:
-            return False
-    return True
+def bogoSort(a):
+    while (is_sorted(a) == False):
+        shuffle(a)
 
-def bogosort(data):
-    while not is_sorted(data):
-        random.shuffle(data)
-    return data
+def is_sorted(a):
+	n = len(a)
+	for i in range(0, n-1):
+		if (a[i] > a[i+1]):
+			return False
+	return True
 
-data = [34,23,122,9]
-bogosort(data)
-print(data)
+def shuffle(a):
+    n = len(a)
+    for i in range(0, n):
+        r = random.randint(0, n-1)
+        a[i], a[r] = a[r], a[i]
+
+a = [3, 2, 4, 1, 0, 5]
+bogoSort(a)
+print("Sorted array :")
+print(a)
+
